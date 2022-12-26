2b27ccf289de93c9ecabba438beda8e8b49c6a08
Rafael Sana Montevechio <rafael_sana@hotmail.com>
2022-12-26 14:35:51 -0300

    fix: :white_check_mark: challenges solved

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -1,5 +1,5 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
   user_id: string;
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const foundUser = this.usersRepository.findById(user_id);
+
+    if (!foundUser) {
+      throw new Error('User not found');
+    }
+
+    const user = this.usersRepository.turnAdmin(foundUser);
+
+    return user;
   }
 }
 
